5d40db49f86c106c803a0d9ac8466ca3368481bd
Vincent Widjaya <vwidjaya@uw.edu>
2019-11-04 00:35:37 -0800

    Add fields, skills, and cities on project creation

--- src/services/project.ts
+++ src/services/project.ts
@@ -9,7 +9,9 @@ export default class ProjectService {
   }
 
   async createProject(project: Project): Promise<void> {
-    const statement = `
+    const { details, fields, skills, locations } = project;
+
+    const insertProject = `
       INSERT INTO project
       VALUES (
         null,
@@ -23,12 +25,43 @@ export default class ProjectService {
       );
     `;
 
-    const params = [project.ownerEmail, project.description, project.startDate, project.endDate];
+    const repeatStatement = (statement: string, count: number): string => {
+      return Array(count)
+        .fill(statement)
+        .join(', ');
+    };
+
+    const insertProjectFields = (projectId: string): string => `
+      INSERT INTO project_field
+      VALUES ${repeatStatement(`(null, ${projectId}, (SELECT field_id FROM field WHERE name = ?))`, fields.length)};
+    `;
+
+    const insertProjectSkills = (projectId: string): string => `
+      INSERT INTO project_skill
+      VALUES ${repeatStatement(`(null, ${projectId}, (SELECT skill_id FROM skill WHERE name = ?))`, skills.length)};
+    `;
+
+    const insertProjectCities = (projectId: string): string => `
+      INSERT INTO project_city
+      VALUES ${repeatStatement(`(null, ${projectId}, (SELECT city_id FROM city WHERE name = ?))`, locations.length)};
+    `;
+
+    const projectParams = [details.ownerEmail, details.description, details.startDate, details.endDate];
+    const conn = await this.db.getConnection();
 
     try {
-      await this.db.execute(statement, params);
+      conn.beginTransaction();
+      const [projectRes] = await conn.execute(insertProject, projectParams);
+      const projectId = projectRes['insertId'];
+      await conn.execute(insertProjectFields(projectId), fields);
+      await conn.execute(insertProjectSkills(projectId), skills);
+      await conn.execute(insertProjectCities(projectId), locations.map(l => l.city));
+      await conn.commit();
+      conn.release();
     } catch (err) {
       console.log(`error creating new project: ${err}`);
+      await conn.rollback();
+      conn.release();
     }
   }
 }
--- src/types.ts
+++ src/types.ts
@@ -13,10 +13,23 @@ export type Student = {
 };
 
 export type Project = {
+  details: ProjectDetails;
+  fields: string[];
+  skills: string[];
+  locations: Location[];
+};
+
+export type ProjectDetails = {
   _id?: string;
   ownerEmail: string;
   description: string;
   startDate: Date;
   endDate?: Date;
   status?: string;
+};
+
+export type Location = {
+  city: string;
+  state?: string;
+  country: string;
 };
